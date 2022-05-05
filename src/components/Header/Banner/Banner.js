961283100f2f8aa1861b17238bcadae545e85ede
Abdullah <mdabdullahnoman777@gmail.com>
2022-05-05 17:02:37 +0600

    Banner Section Add

--- src/components/Header/Banner/Banner.js
+++ src/components/Header/Banner/Banner.js
@@ -1,11 +1,28 @@
-import React from 'react';
-
+import React from "react";
+import Slide from "../../../image/slide.png";
+import banner from "../../../image/banner.png";
+import "./Banner.css";
 const Banner = () => {
-    return (
-        <div>
-            
+  return (
+    <div>
+      <div className="banner-container">
+        <div className="banner-details font-mono">
+          <img className="silde" src={Slide} alt="" />
+          <h1 className="text-white text-4xl pl-20 pb-4 font-bold">
+            Best <span>Luxurious Cars</span> Here
+          </h1>
+          <h2 className="text-6xl  pl-20 pb-4 font-semibold"><span>Popular Vehicle Dealership</span></h2>
+          <p className="text-white text-xl pl-20 pb-4">
+            Find the <span>Perfect Car</span> With Our Car Finder Tool
+          </p>
+          <button className="ml-20 banner-btn">About</button>
         </div>
-    );
+        <div className="banner-image">
+          <img src={banner} alt="" />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Banner;
