a8d1fa8ef6a04a3264a8dbe751e2017123793758
Abdullah <mdabdullahnoman777@gmail.com>
2022-05-06 08:25:47 +0600

    inventory responsive

--- src/components/Products/Products.js
+++ src/components/Products/Products.js
@@ -11,7 +11,9 @@ const Products = () => {
   console.log(product);
   return (
     <section className="products">
-        <h2 className="text-white text-center text-5xl font-semibold pb-20 font-mono">See Our Pro<span>ducts</span></h2>
+      <h2 className="text-white text-center text-2xl md:text-4xl font-semibold pb-20 font-mono">
+        Latest Veh<span>icles</span> on Sale
+      </h2>
       <div className="product-container">
         {product.slice(0, 6).map((item) => (
           <div className="product-info relative" key={item._id}>
@@ -19,16 +21,27 @@ const Products = () => {
               <img src={item.img} alt="" />
             </div>
             <div className="item-details font-mono mt-5">
-              <h1 className="text-white pb-2 px-5 text-lg"><span className="text-xl">Name:</span> {item.name}</h1>
-              <h2 className="text-white pb-2 px-5 text-lg"><span className="text-xl">Price:</span> {item.price}</h2>
-              <h3 className="text-white pb-2 px-5 text-lg"><span className="text-xl">Quantity:</span> {item.quantity}</h3>
-              <p className="text-white pb-2 px-5 text-lg"><span className="text-xl">Supplier:</span> {item.supplier}</p>
-              <p className="text-white pb-2 text-sm text-justify px-5 mb-20">{item.description}</p>
+              <h1 className="text-white pb-2 px-5 text-lg">
+                <span className="text-xl">Name:</span> {item.name}
+              </h1>
+              <h2 className="text-white pb-2 px-5 text-lg">
+                <span className="text-xl">Price:</span> {item.price}
+              </h2>
+              <h3 className="text-white pb-2 px-5 text-lg">
+                <span className="text-xl">Quantity:</span> {item.quantity}
+              </h3>
+              <p className="text-white pb-2 px-5 text-lg">
+                <span className="text-xl">Supplier:</span> {item.supplier}
+              </p>
+              <p className="text-white pb-2 text-sm text-justify px-5 mb-20">
+                {item.description}
+              </p>
             </div>
             <button className="absolute bottom-0 update-btn">Up Date</button>
           </div>
         ))}
       </div>
+      <h1 className="manage-btn font-mono">Manage Inventory</h1>
     </section>
   );
 };
